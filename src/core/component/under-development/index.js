b929c4fb47c19aa71af2e088dca0cceb03a96f02
tijo-thomaz <tijo1293@gmail.com>
2022-10-01 03:48:57 +0530

    color changed

--- src/core/component/under-development/index.js
+++ src/core/component/under-development/index.js
@@ -12,8 +12,8 @@ const UnderDevelopment = () => {
                 Developer
             </h2>
             <h3>This Page is under development </h3>
-            <span className="know-more">Know more about me
-                <a href="https://www.linkedin.com/in/tijo-j-thomaz93/" target="_blank" rel="noopener noreferrer"
+            <span className="know-more">Know more about me{"  "}
+                <a style={{"color":"#01d85f"}}href="https://www.linkedin.com/in/tijo-j-thomaz93/" target="_blank" rel="noopener noreferrer"
                 >here</a>
             </span>
         </div><img src="https://imgs.search.brave.com/vVBK0wtFvaj8rWyjaiWF34P2N63oofSK8h1fS-tJBBk/rs:fit:1200:737:1/g:ce/aHR0cDovL3d3dy5i/bGFja3JvY2tzdHVk/aW8uY2Evd3AtY29u/dGVudC91cGxvYWRz/LzIwMTYvMTAvY29t/aW5nLXNvb24tbWVn/YS5wbmc" /></>
