dcef94f74c6064f57ba4416474be8378e28ec056
tijo-thomaz <tijo1293@gmail.com>
2022-10-09 05:29:17 +0530

    new style added

---
+++ src/data/static-data.js
@@ -0,0 +1,68 @@
+export const dataItem = {
+    description: 'Passionate Front-End Engineer with 5+ years of experience, with a goal to find solutions  for different domains with my expertise and meet the requirements of the organization. Focused on  writing optimized and good quality code and trying to deliver client satisfaction work. An enthusiastic  learner who quickly grasps new concepts and technical skills.',
+    education: [{
+        id: 1,
+        date: "2014-2017",
+        course: 'Master of Computer Applications',
+        university: 'M.G University',
+        college: 'marian college kuttikanam',
+        description: ""
+    },
+    {
+        id: 2,
+        date: "2011-2014",
+        course: 'Bachelor of Computer Applications',
+        university: 'M.G University',
+        college: 'Parumala Mar Gregorios College',
+        description: ""
+    },
+    {
+        id: 3,
+        date: "2009-2011",
+        course: 'Higher Secondary',
+        university: 'Kerala Board, India',
+        college: 'GHSS Thottakonam',
+        description: ""
+    },
+    {
+        id: 4,
+        date: "2000-2009",
+        course: 'High School',
+        university: 'Kerala Board, India',
+        college: 'St. Thomas EMHS Pandalam',
+        description: ""
+    }
+],
+    experience: [{
+        id: 1,
+        date: "2021-present",
+        designation: 'Senior Associate Consultant',
+        company: 'Infosys',
+        description: "test"
+    },
+    {
+        id: 2,
+        date: "2020-2021",
+        designation: 'Software Engineer',
+        company: 'NetObjex',
+        description: "test"
+    },
+    {
+        id: 3,
+        date: "2018-2020",
+        designation: 'Engineer',
+        company: 'Qburst',
+        description: "test"
+    },
+    {
+        id: 4,
+        date: "2016-2018",
+        designation: 'Associate Trainee',
+        company: 'Speridian',
+        description: "test"
+    }
+]
+
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
+import './assets/scss/styles.scss'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
