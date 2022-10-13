296525fc5cef7917dff2de1e3cb85b6b512af7b0
tijo-thomaz <tijo1293@gmail.com>
2022-10-13 23:27:05 +0530

    removed extra router

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import { Routes, Route, BrowserRouter, } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
-function RouterModule({ navActive, imgSrc, onNavButtonClick }) {
+function RouterModule({ navActive, imgSrc ,onNavButtonClick}) {
     return (
         <React.Fragment>
-            < BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home navActive={navActive} imgSrc={imgSrc} onNavButtonClick={onNavButtonClick} />} />
-                    <Route path="/about" element={<About navActive={navActive} />} />
-                    {/* <Route path="/contact" element={<Contact navActive={navActive} />} /> */}
-                </Routes>
-            </BrowserRouter>
+            <Routes>
+                <Route path="/"  element={<Home navActive={navActive} imgSrc={imgSrc}  onNavButtonClick={onNavButtonClick}/>} />
+                <Route path="/about" element={<About navActive={navActive} />} />
+                {/* <Route path="/contact" element={<Contact navActive={navActive} />} /> */}
+            </Routes>
         </React.Fragment>
     )
 }
